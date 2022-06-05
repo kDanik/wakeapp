822766425f3b94e18d7f7cfc57c6bf4fa66c71c1
kDanik <daniilkurachkin@gmail.com>
2022-06-05 02:21:38 +0200

    delete test controller

--- src/main/java/de/wakeapp/controller/TestController.java
+++
@@ -1,8 +0,0 @@
-package de.wakeapp.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class TestController {
-}
