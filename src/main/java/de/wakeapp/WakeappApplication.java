6b891be9c25b8f056719456899e98ff872b604f7
kDanik <daniilkurachkin@gmail.com>
2022-06-16 13:07:36 +0200

    added values for messages_de.properties and location change

--- src/main/java/de/wakeapp/WakeappApplication.java
+++ src/main/java/de/wakeapp/WakeappApplication.java
@@ -2,14 +2,42 @@ package de.wakeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableJpaRepositories("de.wakeapp.repository")
-public class WakeappApplication {
+public class WakeappApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WakeappApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+		// change value here to change default language
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		// Defaults to "locale" if not set
+		localeChangeInterceptor.setParamName("localeData");
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+		interceptorRegistry.addInterceptor(localeChangeInterceptor());
+	}
 }
